5702dc963e9e2334c5e36733e5520bbac49a1342
Josiah Henson <pbinspanish@outlook.com>
2025-08-28 14:17:02 -0400

    update site description const

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Josiah Henson";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION =
+  "Reality is a lovely place, but I wouldn't want to live there.";
